b8e2355636bea71634e90958608e2172f5ec88cf
Kasim <muhammad.k.ali.93@gmail.com>
2023-04-23 15:03:01 +0100

    added array.js

---
+++ javascript-beginner/array.js
@@ -0,0 +1,11 @@
+let countries = ['Vietnam', 'China', 'USA', 'Japan', 'Brazil', 'Mexico', 'Canada']
+
+// assign and indexing, array is mutable
+countries[0] = 'Vietnam is GREAT'
+console.log(countries)
+
+// string is immutable
+let myName = 'Kasim'
+myName[0] = 'Q'
+console.log(myName[0])
+
