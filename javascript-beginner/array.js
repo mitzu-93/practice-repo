4a924663aa7753be04ef10f50c95fe7408ae22a1
Kasim <muhammad.k.ali.93@gmail.com>
2023-04-23 19:01:11 +0100

    implement array iteration

--- javascript-beginner/array.js
+++ javascript-beginner/array.js
@@ -9,3 +9,39 @@ let myName = 'Kasim'
 myName[0] = 'Q'
 console.log(myName[0])
 
+// number of items in array
+console.log(countries.length)
+
+// array
+var myArr = ['one', 'two', 'three']
+
+// python .append
+myArr.push('four')
+console.log(myArr)
+
+// same as python list pop
+myArr.pop()
+console.log(myArr)
+
+//array last index
+console.log(myArr[myArr.length - 1])
+
+//array iteration
+var arr = ['A', 'B', 'C', 'D', 'E']
+for(var i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+}
+
+// more like python for loop
+for (letter of arr){
+    console.log(letter);
+}
+
+// forEach iteration + func
+function addAwesome(name){
+    console.log(name + ' is awesome!')
+}
+
+let namesArr = ['Kasim', 'Hashim', 'Ali', 'Sara']
+
+namesArr.forEach(addAwesome)
