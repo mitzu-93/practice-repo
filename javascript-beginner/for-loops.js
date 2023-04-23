7d2de4bd72cf7a6ce6b1b88a68180bdae23c9960
Kasim <muhammad.k.ali.93@gmail.com>
2023-04-23 14:36:42 +0100

    added for loops and function files

---
+++ javascript-beginner/for-loops.js
@@ -0,0 +1,15 @@
+for (var i=0; i < 11; i++) {
+    if (i % 2 == 0) {
+        console.log(i)
+    }
+    else if (i % 3 == 0) {
+        console.log(i + " is divisible by 3")
+    }
+}
+
+var x = 'string'
+xLength = x.length
+
+for (var y = 0; y < xLength; y++) {
+    console.log(x[y])
+}
